e2728026daabb802f800fb9ccfd6eea0b7eac06d
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-11 13:53:12 -0700

    Add user and session to Rails backend. Initialize Backbone on document ready.

---
+++ app/assets/javascripts/vino_bueno.js
@@ -0,0 +1,9 @@
+window.VinoBueno = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,53 @@
+class SessionsController < ApplicationController
+
+  def new
+    render :signin
+  end
+
+  def create
+
+    # Inserted into params based on button clicked
+    action = params[:type]
+
+    if action == 'sign-in'
+
+      user = User.find_by_credentials(
+                    params[:user][:username],
+                    params[:user][:password])
+
+      if user
+        login!(user)
+        redirect_to root_url
+      else
+        flash.now[:errors] = ["Invalid username or password"]
+        render :signin
+      end
+
+    elsif action == 'sign-up'
+
+      @user = User.new(user_params)
+
+      if @user.save
+        login!(@user)
+        redirect_to root_url
+      else
+        flash.now[:errors] = @user.errors.full_messages
+        redirect_to root_url
+      end
+
+    end
+
+  end
+
+  def destroy
+    logout!
+    render :signin
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
+end
