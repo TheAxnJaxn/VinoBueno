d854dda337dbdb67182df7f1a71e174316a1b12e
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-12 09:42:25 -0700

    Fixing sign in / sign up.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
-    render :signin
+    render :signinup
   end
 
   def create
-
-    # Inserted into params based on button clicked
-    action = params[:type]
-    if action == 'sign-in'
-
-      user = User.find_by_credentials(
+    user = User.find_by_credentials(
                     params[:user][:username],
                     params[:user][:password])
 
@@ -19,28 +14,13 @@ class SessionsController < ApplicationController
         redirect_to root_url
       else
         flash.now[:errors] = ["Invalid username or password"]
-        render :signin
+        render :signinup
       end
-
-    elsif action == 'sign-up'
-
-      @user = User.new(user_params)
-
-      if @user.save
-        login!(@user)
-        redirect_to root_url
-      else
-        flash.now[:errors] = @user.errors.full_messages
-        redirect_to root_url
-      end
-
-    end
-
   end
 
   def destroy
     logout!
-    render :signin
+    render :signinup
   end
 
   private
