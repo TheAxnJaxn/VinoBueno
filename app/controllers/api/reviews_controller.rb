246fe6273a77ba0c84eaea8a65ac2973a9ea8081
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-14 10:54:47 -0700

    Add api/reviews controller for creating and destroying a review.

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -2,7 +2,25 @@ class Api::ReviewsController < ApplicationController
 
   before_action :require_logged_in!
 
-  def index
+  def destroy
+    @review = Review.find(params[:id])
+    @review.try(:destroy)
+    render json: {}
   end
-  
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      render json: @review
+    else
+      render json: @review.errors.full_messages, status: :unprocessable_entity
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:user_id, :wine_id, :rating, :review)
+  end
+
 end
