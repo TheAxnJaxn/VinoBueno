d08e694aea0a90870a8654f56669ad08ab950c1c
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-20 23:22:01 -0700

    Set a shortened description as an attribute on the wine model in the cellar, to display a shorter description when hovering over that wine in the cellar.

--- app/assets/javascripts/views/cellars/cellar-wine.js
+++ app/assets/javascripts/views/cellars/cellar-wine.js
@@ -6,12 +6,21 @@ VinoBueno.Views.CellarWine = Backbone.View.extend({
 
   className: 'wine-in-cellar',
 
+  initialize: function () {
+    this.setMiniDescription();
+  },
+
   render: function () {
     var content = this.template({
       wine: this.model
     });
     this.$el.html(content);
     return this;
+  },
+
+  setMiniDescription: function () {
+    var mini = this.model.get('description').substring(0, 101).concat('...');
+    this.model.set('mini-descr', mini);
   }
 
 });
