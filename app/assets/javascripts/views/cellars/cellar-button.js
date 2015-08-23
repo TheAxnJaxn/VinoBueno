b57a3ba0e7463b3c14722048bab2284585bf4514
Krista Jackson <krista.lynn.jackson@gmail.com>
2015-08-22 18:42:53 -0700

    Cellar button adds removal option at the bottom, only if the wine is currently in one of the current user's cellars.

--- app/assets/javascripts/views/cellars/cellar-button.js
+++ app/assets/javascripts/views/cellars/cellar-button.js
@@ -19,12 +19,13 @@ VinoBueno.Views.CellarButton = Backbone.View.extend ({
 
   addCellarOptions: function () {
     this.collection.each(this.addCellarToButton.bind(this));
+    this.addRemovalOption();
   },
 
   addCellarToButton: function (cellar) {
     if ( cellar.get('wine_ids').indexOf(parseInt(this.wine.id)) > -1 ) {
       // replace "Add to Cellar" with current cellar
-      this.$('.current-cellar').text(cellar.escape('name'))
+      this.$('.current-cellar').text(cellar.escape('name'));
     } else {
       // add all other cellars to dropdown li options
       var $a = $('<a href="#"></a>')
@@ -32,6 +33,15 @@ VinoBueno.Views.CellarButton = Backbone.View.extend ({
                 .text(cellar.escape('name'));
       var $el = $('<li></li>').append($a);
       this.$('.dropdown-menu').append($el);
+    }
+  },
+
+  addRemovalOption: function () {
+    // adds removal option if wine is in a cellar
+    if (this.$('.current-cellar').text() !== "Add to Cellar") {
+      var $line = $('<li role="separator" class="divider"></li>')
+      var $el = $('<li><a href="#">Remove From Cellar</a></li>')
+      this.$('.dropdown-menu').append($line).append($el);
     }
   }
 
